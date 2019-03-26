18aeba2b0efd3b1d0bec12a0f4a4d800385176b2
Abizar <babyzar97@gmail.com>
2019-03-26 02:57:33 -0500

    backup

---
+++ Oscillations Unity 06-29-18/Assets/updateRoot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateRoot : StateMachineBehaviour
+{
+
+	private updateRootPos script;
+	private GameObject ethan;
+
+	private bool doneOnce = false;
+	
+	void onEnable()
+	{
+		
+	}
+	
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		/*
+		if(script == null)
+			script = animator.gameObject.GetComponent<updateRootPos>();
+
+		if (!doneOnce)
+		{
+			Vector3 hipsDiff = script.currHipsPos - script.ethanHips.transform.localPosition;
+			Vector3 newPos = new Vector3(hipsDiff.x, script.ethan.transform.position.y, hipsDiff.z);
+			Vector3 ran = new Vector3(5, 0, 0);
+			GameObject ethanHips = GameObject.Find("EthanHips");
+			//script.ethan.transform.position = newPos;
+			doneOnce = true;
+		}
+		*/
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	
+	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if (stateInfo.normalizedTime > .97f && !animator.IsInTransition(layerIndex))
+		if(script == null)
+			script = animator.gameObject.GetComponent<updateRootPos>();
+		//script.animation_finished = true;
+		Debug.Log("Im an idiot");
+
+	}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ Oscillations Unity 06-29-18/Assets/updateRootPos.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateRootPos : MonoBehaviour
+{
+	public GameObject ethan;
+	public Vector3 currHipsPos;
+	public Quaternion currHipsRot;
+	private Animator anim;
+	public GameObject ethanHips;
+	public Vector3 check;
+
+	public Vector3 oldRoot;
+	// This var will determine if the animation is started
+	public bool animation_started = false;
+	// This var will determine if the animation is finished
+	public bool animation_finished = true;
+
+	private int animChange;
+	
+ 
+	// Use this for initialization
+	void Start()
+	{
+		ethan = GameObject.Find("morro_man");
+		anim = ethan.GetComponent<Animator>();
+		ethanHips = ethan.transform.Find("MHuman_Root").gameObject;
+		animation_started = true;
+		animation_finished = false;
+		oldRoot = ethan.transform.position;
+	}
+
+ 
+	/* This function is trigger at the end of the animation */
+	public void animationFinished()
+	{
+		animation_finished = true;
+	}
+
+	public void animationStarted(int intKeyframe)
+	{
+		
+		animation_started = true;
+		animChange = intKeyframe;
+	}
+ 
+	/*  
+	    At the end of the frame if the animation is finished
+	    we update the position of the parent to the last position of the child
+	    and set the position of the child to zero inside the parent.
+	*/
+	void LateUpdate () {
+		
+		check = ethanHips.transform.position;
+		
+		if (animation_finished)
+		{
+			currHipsPos = ethanHips.transform.position;
+			currHipsRot = ethanHips.transform.rotation;
+			animation_finished = false;
+			Debug.Log(currHipsPos);
+		}
+			
+		Debug.Log("Im an idiot");
+		
+		
+		if(animation_started)
+		{
+			animation_started = false;
+			//Vector3 hipsDiff = currHipsPos - ethanHips.transform.localPosition;
+			//Vector3 newPos = new Vector3(hipsDiff.x, ethan.transform.position.y, hipsDiff.z);
+			Vector3 ran = new Vector3(5, 0, 0);
+			//Vector3 newPos = currHipsPos;
+			Vector3 newPos = currHipsPos;
+			newPos = new Vector3(newPos.x, 0f, newPos.z);
+
+			Vector3 eulerRot = currHipsRot.eulerAngles;
+			//var lookPos = newPos - ethan.transform.localPosition;
+			var lookPos = newPos;
+			lookPos.y = 0;
+			var rotation = Quaternion.LookRotation(lookPos);
+
+			Vector3 newRot = ethan.transform.rotation.eulerAngles;
+			eulerRot = new Vector3(newRot.x, eulerRot.y, newRot.z);
+			
+			//ethan.transform.position += newPos - ethanHips.transform.localPosition;
+			
+			
+			//ethan.transform.rotation = Quaternion.LookRotation(lookPos);
+
+			switch (animChange)
+			{
+				case 3:
+					//ethan.transform.position += new Vector3(0.1535187f - 0.6198351f, 0f, 0.07261555f + 0.2550304f );
+					break;
+				case 4:
+					ethan.transform.position += new Vector3(+0.001823157f-0.1383394f, 0f, +0.2458937f -1.364185f);
+					Debug.Log("reached");
+					break;
+					// 
+					// 
+				case 7:
+					ethan.transform.position += new Vector3(0.1297451f, 0f, 0.105484f);
+					Debug.Log("YYYYE");
+					break;
+				case 8:
+					
+					Debug.Log("uh man");
+					break;
+				case 9:
+					
+					//-1.324282
+				//-0.593114
+				
+				//-0.2297451
+				//-1.405484
+				default:
+					break;
+			}
+		
+			
+			
+			//else 
+			
+			//
+		}
+	}
+
+	private void OnAnimatorMove()
+	{
+
+	}
+	
+	
+}
